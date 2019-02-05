6fb12f6e2cc0cefdbeded96c23f77651659db523
Matteo Battisti <matteobattisti@protonmail.com>
2019-02-05 22:31:32 +0100

    first commit

---
+++ app.ts
@@ -0,0 +1,31 @@
+const cheerio = require('cheerio');
+const rp = require('request-promise');
+
+
+function scrap(channel:string, message:number) {
+	let options = 
+	{
+		uri: 'http://t.me/${channel}/${message}?embed=1',
+		method: 'POST',
+		transform: body => cheerio.load(body);
+	}
+
+	rp(options)
+		.then(($, response) => {
+			if (response.statusCode == 200) {
+				let user:string = $('div.tgme_widget_message_author')
+				let message:string  = $('div.tgme_widget_message_text')
+				let data = 
+					{
+						usr: user,
+						msg: message,
+					}
+			};
+
+		)}
+		.catch((e:string) => console.err(e););
+	return data;
+
+}
+
+scrap("pythonita", 7)
