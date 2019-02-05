0645d25f9e1f79a46e509ffd58a870151419be87
Matteo Battisti <matteobattisti@protonmail.com>
2019-02-05 23:29:45 +0100

    it finally works! thanks desi

--- app.ts
+++ app.ts
@@ -2,30 +2,27 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 
 
-function scrap(channel:string, message:number) {
-	let options = 
+function scrap(channel: string, message: number) {
+	let options =
 	{
 		uri: 'http://t.me/${channel}/${message}?embed=1',
 		method: 'POST',
-		transform: ((body) => cheerio.load(body)),
+		transform: body => cheerio.load(body)
 	}
 
 	rp(options)
-		.then(($) => {
+		.then(($, response) => {
 			if (response.statusCode == 200) {
-				let user:string = $('div.tgme_widget_message_author')
-				let message:string  = $('div.tgme_widget_message_text')
-				let data = 
-					{
-						usr: user,
-						msg: message,
-					}
-			};
-			console.err('error: channel ${channel} not found');
-		});
-		.catch((e:string) => console.err(e););
-	return data;
-
+				let user: string = $('div.tgme_widget_message_author')
+				let message: string = $('div.tgme_widget_message_text')
+				let data =
+				{
+					usr: user,
+					msg: message,
+				}
+			}
+		})
+		.catch((e: string) => console.error(e));
 }
 
 scrap("pythonita", 7)
